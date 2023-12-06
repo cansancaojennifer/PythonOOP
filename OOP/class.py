05aa6c7f30c2f44bfe7469a122b8401fc22651bd
robotictitans <120498502+cansancaojennifer@users.noreply.github.com>
2023-12-06 13:48:46 -0300

    anit method

--- OOP/class.py
+++ OOP/class.py
@@ -1,12 +1,16 @@
 class Dog:
 
+    def __init__(self, name):
+        self.name = name
+        print(name)
+
     def add_one(self, x):
         return x + 1
 
     def bark(self):
         print("bark")
 
-d = Dog()
+d = Dog("Snow")
 d.bark()
 print(d.add_one(5))
 
