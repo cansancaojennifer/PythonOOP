157baab57aee19bfba5fac0ef1f298af8965d68a
robotictitans <120498502+cansancaojennifer@users.noreply.github.com>
2023-12-06 14:23:44 -0300

    modifying attributes

--- OOP/class.py
+++ OOP/class.py
@@ -9,6 +9,9 @@ class Dog:
     
     def get_age(self):
         return self.age
+    
+    def set_age(self, age):
+        self.age = age
 
     def add_one(self, x):
         return x + 1
@@ -20,6 +23,7 @@ d = Dog("Snow", 6)
 print(d.get_name())
 print(d.get_age())
 d2 = Dog("Butter", 9)
+d2.set_age(12)
 print(d2.get_name())
 print(d2.get_age())
 
