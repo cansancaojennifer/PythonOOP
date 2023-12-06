aed87fb4b3b86e7ab67f1cfefdbf87b61feb1ce7
robotictitans <120498502+cansancaojennifer@users.noreply.github.com>
2023-12-06 13:53:17 -0300

    attributes

--- OOP/class.py
+++ OOP/class.py
@@ -1,8 +1,14 @@
 class Dog:
 
-    def __init__(self, name):
+    def __init__(self, name, age):
         self.name = name
-        print(name)
+        self.age = age
+
+    def get_name(self):
+        return self.name
+    
+    def get_age(self):
+        return self.age
 
     def add_one(self, x):
         return x + 1
@@ -10,7 +16,13 @@ class Dog:
     def bark(self):
         print("bark")
 
-d = Dog("Snow")
+d = Dog("Snow", 6)
+print(d.get_name())
+print(d.get_age())
+d2 = Dog("Butter", 9)
+print(d2.get_name())
+print(d2.get_age())
+
 d.bark()
 print(d.add_one(5))
 
